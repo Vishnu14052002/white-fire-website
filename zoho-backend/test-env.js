baf965afefdb21693b7a51a71253712266555aa3
Vishnu14052002 <vishnug14052002@gmail.com>
2025-05-22 13:16:56 +0100

    dummy commit

--- zoho-backend/test-env.js
+++ zoho-backend/test-env.js
@@ -1,6 +1,1 @@
-// test-env.js
-require('dotenv').config();
-
-console.log('Client ID:', process.env.ZOHO_CLIENT_ID);
-console.log('Client Secret:', process.env.ZOHO_CLIENT_SECRET);
-console.log('Redirect URI:', process.env.ZOHO_REDIRECT_URI);
+t
